16bc634ac3349d3dc6785b9cb39a35f209856cb7
yutakei <chorichori55@outlook.com>
2020-02-25 13:26:19 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+        }
+    }
+}

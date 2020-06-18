163049aa8515865b6d02d5214dfacaa7d6d8b3ea
yutakei <chorichori55@outlook.com>
2020-06-18 14:08:55 +0900

    2020/06/18version

--- MyClass.cs
+++ MyClass.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         public string ReturnMessage()
         {
-            return "Happy coding! yukie takei :-) 2020/04/16 New Version!!!";
+            return "Happy coding! yukie takei :-) 2020/06/18 New Version!!!!";
         }
     }
 }
